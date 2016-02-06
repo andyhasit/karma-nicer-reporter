829ae0d1f4eff841021d7bbaa7db61dd9616ec76
Andrew Buchan <andyhasit@gmail.com>
2016-02-06 11:26:11 +0000

    Improvements

--- example_tests/example.spec.js
+++ example_tests/example.spec.js
@@ -1,12 +1,80 @@
 
+
 describe('Suite that passes', function() {
-  
   it('2 plus 2 equals 4', function() {
     expect(2 + 2).toEqual(4);
   });
-  
   it('3 plus 3 equals 6', function() {
     expect(3 + 3).toEqual(6);
   });
-  
+  /*Just add loads the same to show test count padding in print output
+   2  12  7
+  11   6  7
+  */
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('4 plus 4 equals 8', function() {
+    expect(4 + 4).toEqual(8);
+  });
+});
+
+
+describe('Suite that passes with test skipped', function() {
+  it('2 plus 2 equals 4', function() {
+    expect(2 + 2).toEqual(4);
+  });
+  xit('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  xit('4 plus 4 equals 8', function() {
+    expect(4 + 4).toEqual(8);
+  });
+});
+
+describe('Suite that fails', function() {
+  it('2 plus 2 equals 4', function() {
+    expect(2 + 2).toEqual(4);
+  });
+  it('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('99 plus 1 equals 2', function() {
+    expect(99 + 1).toEqual(2);
+  });
+});
+
+describe('Suite that fails with test skipped', function() {
+  it('2 plus 2 equals 4', function() {
+    expect(2 + 2).toEqual(4);
+  });
+  xit('3 plus 3 equals 6', function() {
+    expect(3 + 3).toEqual(6);
+  });
+  it('99 plus 1 equals 2', function() {
+    expect(99 + 1).toEqual(2);
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,21 @@
 var gulp = require('gulp');
+var Server = require('karma').Server;
 
-
-task = {
-  name: 'scripts',
-  start: 'src/module.js',
-  sources: 'src/**/*.js',
-  dest: 'dist',
-  outFile: 'relate.min.js'
-}
-
+/**
+ * Copy index.js to node_modules/karma-nicer-reporter so it is used as reporter.
+ * By Karma. See README for details.
+ */
 gulp.task('copy_index', function() { 
   return gulp.src('index.js')
     .pipe(gulp.dest('node_modules/karma-nicer-reporter'));
 });
 
-
-var Server = require('karma').Server;
-
 /**
- * Run test once and exit
+ * Run test once and exit.
  */
-gulp.task('test', function (done) {
+gulp.task('test', ['copy_index'], function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, function() { done(); }).start();
 });
-
-gulp.task('test2', function (done) {
-  karma.server.start({
-    configFile: path.join(__dirname, '/../karma.conf.js'),
-    singleRun: singleRun,
-    autoWatch: !singleRun
-  }, /* wrapping "done" in a function here */ function() { done(); });
-});
-
-function runTests (singleRun, done) {
-  
-}
