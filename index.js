27de477b89b48d35977dbbbbd565cd39bb3f8856
Andrew Buchan <andyhasit@gmail.com>
2016-02-28 00:19:10 +0000

    Better log printing

--- index.js
+++ index.js
@@ -2,34 +2,37 @@ const chalk = require('chalk');
 
 var NicerReporter = function (baseReporterDecorator, config, logger, helper, formatError) {
   baseReporterDecorator(this);
+
   this.adapters = [function(msg) {
-    process.stdout.write.bind(process.stdout)(msg + "\r\n");
+    browserLog(msg);
   }];
-  
+
+
+
+  var currentBrowserName, resultsForSuite, currentSpecName;
   var log = logger.create('reporter.logical');
   var browserCount = 0;
   var horizontalLine = '-----------------------------------------------';
   var startPath = 'at ' + config.protocol + '//' + config.hostname + ':' + config.port + '/';
   var startPathLength = startPath.length;
-  var resultsForSuite; // Set once per browser
   var firstLinePrinted = false;
-  
+
   // Utility functions
-  
+
   function blank() {
     process.stdout.write("\r\n");
   }
-  
+
   function print(msg, color) {
     var color = color || defaulColor;
     console.log(chalk[color](msg));
   }
-  
+
   function write(msg, color) {
     var color = color || defaulColor;
     process.stdout.write(chalk[color](msg));
   }
-  
+
   function z(value) {
     if (value == 0) {
       return ' .'
@@ -37,33 +40,44 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
       return String("  " + value).slice(-2)
     }
   };
-  
+
+  function browserLog(msg) {
+    if (msg.startsWith(currentBrowserName)) {
+      msg = msg.substr(currentBrowserName.length);
+    }
+    //print('Log during spec: ' + currentSpecName);
+    print(msg);
+  }
+
   // Settings from karma.conf.js nicerReporter section
   // chalk colors: black  red  green  yellow  blue  magenta  cyan  white  gray
-  
+
   var reporterConfig = config.nicerReporter || {};
   var successColor = reporterConfig.successColor || 'green';
   var failColor = reporterConfig.failColor || 'red';
   var skipColor = reporterConfig.skipColor || 'yellow';
   var defaulColor = reporterConfig.defaulColor || 'cyan';
   var errorLogColor = reporterConfig.errorLogColor || 'white';
-  
+
   // Karma runner event listeners
-  this.onBrowserStart = function (browsers) {
+  this.onBrowserStart = function (browser) {
+    currentBrowserName = browser.name;
     resultsForSuite = {};
   };
-  
+
   this.onRunStart = function (browsers) {
     print('.');
-    print('..');    
+    print('..');
     print('... running KARMA (with karma-nicer-reporter)');
+    blank();
   };
-  
+
   this.onRunComplete = function () {
     printFinalSummary();
   };
-  
+
   this.onSpecComplete = function(browser, result) {
+    currentSpecName = result.description;
     var suite = result.suite;
     if (resultsForSuite[suite] === undefined) {
       resultsForSuite[suite] = [];
@@ -85,9 +99,9 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
     }
     printBrowserSummary(browser);
   };
-  
+
   // Worker functions
-  
+
   function printSuiteSummary(suiteName, results) {
     function writeSummaryLine(outcome, color) {
       write(' ');
@@ -100,7 +114,7 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
       write(z(len));
       write('  ');
       write(outcome, color);
-      write(' suite > ', color);
+      write(' > ', color);
       write(suiteName , color);
       write('\r\n');
     }
@@ -129,7 +143,7 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
       writeSummaryLine('FAILED', failColor);
     }
   };
-  
+
   function printSpecFailure(result){
     blank();
     if (!firstLinePrinted) {
@@ -163,7 +177,7 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
       });
     });
   }
-  
+
   function printBrowserSummary(browser) {
     var scores = browser.lastResult;
     blank();
@@ -177,7 +191,7 @@ var NicerReporter = function (baseReporterDecorator, config, logger, helper, for
     write('    TOTAL: ' + scores.total + ' (time: ' + scores.totalTime + 'sec)');
     blank();
   }
-  
+
   function printFinalSummary() {
     blank();
     print('-------');
